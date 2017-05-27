f07cd41af906bfcc3e79b9881887106f57d7dc27
simoneas02 <simoneas02@hotmail.com>
2017-05-27 15:18:35 -0300

    starting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App-header">
+          <h2>TO DO List</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
