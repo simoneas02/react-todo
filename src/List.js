8be7646e9c74ef26a3074713747d3b4fed85fde9
Simone Amorim Silva <simoneas02@hotmail.com>
2017-05-29 19:47:33 -0300

    add function editItem

--- src/List.js
+++ src/List.js
@@ -4,20 +4,23 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: []
+      list: [],
+      isEditable: false
     };
   };
 
   addItem() {
     const newText = this.refs.text;
     const itemList = this.state.list;
+    const isEditable = this.state.isEditable;
     const index = itemList.length + 1;
      
       if(newText.value !== "") {
         itemList.push(
           {
             id: index,
-            text: newText.value
+            text: newText.value,
+            isEditable: false
           }
         );
     };
@@ -33,15 +36,63 @@ class List extends Component {
 
     this.setState({ list: itemList });
   };
- 
+
+  editItem(id) {
+    const itemList = this.state.list.map((item) => {
+      if(item.id === id) {
+        return (
+          {
+            isEditable: true,
+            id: item.id,
+            text: item.text
+          }
+        )
+      } else {
+        return (
+          {
+            isEditable: false,
+            id: item.id,
+            text: item.text
+          }
+        )
+      }
+    });
+    this.setState({ list: itemList} );
+  }
+
+  updateItem(id) {
+    const value = this.refs[id];
+
+    const itemList = this.state.list;
+
+
+ };
+
+  saveItem(id) {
+
+  }
+
   render() {
     const itemList = this.state.list.map((item)  => {
-      return (
-        <li>
-          { item.text } 
-          <button onClick={ this.removeItem.bind(this, item.id) }>x</button>
-        </li>
-      );
+      if(item.isEditable) {
+        return (
+            <li>
+              <input type="text"
+                ref={ item.id }
+                value={ item.text }
+                onChange={ this.updateItem.bind(this, item.id) }/>
+                <button onClick={ this.saveItem.bind(this, item.id) }>SAVE</button>
+            </li>
+          )
+      } else {
+        return (
+          <li>
+              { item.text } 
+              <button onClick={ this.removeItem.bind(this, item.id) }>DELETE</button>
+              <button onClick={ this.editItem.bind(this, item.id) }>EDITE</button>
+            </li>
+        )
+      }
     });
 
     return (
